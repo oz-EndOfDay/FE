7ee98de2b5583800410d15b541194094b1f04293
원도훈 <dnjsehgns98@gmail.com>
2024-12-27 10:43:27 +0900

    feat: 프로필 헤더 UI 수정

--- endofday/src/app/layout.tsx
+++ endofday/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import React from "react";
 import Sidebar from "@/components/ui/Sidebar";
+import Header from "@/components/ui/Header";
 export default function RootLayout(props: {children: React.ReactNode}) {
     const {children} = props;
     return (
@@ -8,6 +9,7 @@ export default function RootLayout(props: {children: React.ReactNode}) {
             <body>
                 <div className="flex h-screen">
                     <Sidebar />
+                    <Header />
                     <main className="flex-1 p-6 overflow-y-auto">{children}</main>
                 </div>
             </body>
