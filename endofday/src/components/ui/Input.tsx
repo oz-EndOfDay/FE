c4e7f0d26a5c447ac486ceb283ed82c29e88a970
Sangwoo Shin <ssangwoo96@gmail.com>
2024-12-27 14:39:30 +0900

    init 유효성 검사 패키지 설치(react-hook-form, zod)

--- endofday/src/components/ui/Input.tsx
+++ endofday/src/components/ui/Input.tsx
@@ -1,32 +1,40 @@
 type InputProps = {
-    label: string;
-    type: string;
-    value?: string;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    placeholder: string;
-    className?: string;
-    id: string;
+  label: string;
+  type: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  className?: string;
+  id: string;
+  required: boolean;
 };
 
-const Input: React.FC<InputProps> = ({id, label, type, value, onChange, placeholder, className}) => {
-    return (
-        <div className={`flex flex-col ${className}`}>
-            <label
-                className="mb-2 text-sm text-gray"
-                htmlFor={id}
-            >
-                {label}
-            </label>
-            <input
-                id={id}
-                type={type}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-                className="p-3 rounded-lg bg-[#F2F4F8] placeholder:text-gray text-black focus:outline-none"
-            />
-        </div>
-    );
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  value,
+  onChange,
+  placeholder,
+  className,
+  required,
+}) => {
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <label className="mb-2 text-sm text-gray" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="p-3 rounded-lg bg-[#F2F4F8] placeholder:text-gray text-black focus:outline-none"
+        required={required}
+      />
+    </div>
+  );
 };
 
 export default Input;
