d28a85867fd24917bc5129bb8d95fe5c40620f06
Sangwoo Shin <ssangwoo96@gmail.com>
2024-12-31 15:14:17 +0900

    feat:비밀번호 재설정 페이지 구현

---
+++ endofday/src/app/updatepassword/page.tsx
@@ -0,0 +1,11 @@
+import { UpdatePassword } from "@/components/ui/UpdatePassword";
+
+const updatepassword = () => {
+  return (
+    <div className="flex justify-center items-center h-[90vh]">
+      <UpdatePassword />
+    </div>
+  );
+};
+
+export default updatepassword;
