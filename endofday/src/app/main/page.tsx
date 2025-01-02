88b7468fcb7f47f13d28bc8230c1f0de235b52ea
Sangwoo Shin <ssangwoo96@gmail.com>
2025-01-02 19:48:03 +0900

    feat: 로그인 후 메인 페이지 UI 구현 완료 (도넛 차트 포함)

--- endofday/src/app/main/page.tsx
+++ endofday/src/app/main/page.tsx
@@ -1,4 +1,6 @@
+import EmotionDoughnutChart from "@/components/ui/EmotionDoughnutChart";
 import Heading from "@/components/ui/Heading";
+import Input from "@/components/ui/Input";
 import MainContentBox from "@/components/ui/MainContentBox";
 
 const main = () => {
@@ -23,14 +25,30 @@ const main = () => {
         <MainContentBox
           title="이번 달에 작성된 일기"
           data="12"
-          description={`지난달 대비 n 증가`}
+          description={`지난달 대비 +3 증가`}
         />
         <MainContentBox
           title="가장 많이 작성한 요일"
           data="수요일"
-          description={`평균 작성 횟수: n회/요일`}
+          description={`평균 작성 횟수: 2.3회/요일`}
         />
       </div>
+      <Heading tag="h1" className="text-center">
+        일기를 통해 본 나의 감정 흐름
+      </Heading>
+      <EmotionDoughnutChart />
+      <Input
+        id="week_select"
+        label="주 선택"
+        type="text"
+        placeholder="주 선택"
+      />
+      <Input
+        id="month_select"
+        label="월 선택"
+        type="text"
+        placeholder="월 선택"
+      />
     </div>
   );
 };
