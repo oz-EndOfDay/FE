ffd633109b67bdf6d09efa716a3a891215b31323
Sangwoo Shin <ssangwoo96@gmail.com>
2025-01-09 17:39:11 +0900

    머지 전 커밋

--- endofday/src/api/api.ts
+++ endofday/src/api/api.ts
@@ -1,12 +1,22 @@
 import { LoginFormData } from "@/utils/registrationSchema";
-import axios from "axios";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const login = async (data: LoginFormData) => {
-  const response = await axios.post(
+  const response = await fetch(
     `${API_BASE_URL}/users/login?email=${data.email}&password=${data.password}`,
-    data
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
   );
-  return response.data;
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return await response.json();
 };
