306866ffbf7946266985d1a018128aed15ea05a8
Sangwoo Shin <ssangwoo96@gmail.com>
2025-01-14 20:41:41 +0900

    feat: 사이드바 비로그인 시 클릭 안되게 수정

--- endofday/next.config.mjs
+++ endofday/next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["via.placeholder.com"], // 허용할 도메인 추가
-        domains: ["eod-be.s3.ap-northeast-2.amazonaws.com"], // S3 도메인 추가 (일기 상세조회)
-    },
-    experimental: {
-        optimizePackageImports: ["@chakra-ui/react"],
-    },
+  images: {
+    domains: ["via.placeholder.com"], // 허용할 도메인 추가
+    domains: ["eod-be.s3.ap-northeast-2.amazonaws.com"], // S3 도메인 추가 (일기 상세조회)
+  },
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
--- endofday/src/components/ui/Sidebar.tsx
+++ endofday/src/components/ui/Sidebar.tsx
@@ -16,7 +16,7 @@ const menuItems: MenuItem[] = [
     id: 1,
     label: "Home",
     icon: "/icons/home.svg",
-    path: "/",
+    path: "/main",
   },
   {
     id: 2,
@@ -59,9 +59,13 @@ const Sidebar = () => {
         {menuItems.map((item) => (
           <li key={item.id} className="flex items-center">
             <Link
-              href={item.id === 1 && token ? "/main" : item.path}
-              //토큰이 있으면 메인 클릭시 로그인 페이지가 아닌 메인 페이지로 가게 설정
-              className="flex rounded-xl md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] relative shadow-md justify-center items-center bg-beige"
+              href={item.path}
+              className={
+                token
+                  ? "flex rounded-xl md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] relative shadow-md justify-center items-center bg-beige"
+                  : "flex rounded-xl md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] relative shadow-md justify-center items-center bg-beige pointer-events-none"
+              }
+              //토큰이 없으면 사이드바 클릭이 안되게 설정
             >
               <span className="md:w-[2rem] md:h-[2rem] w-[1.5rem] h-[1.5rem] relative">
                 <Image
