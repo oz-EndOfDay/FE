2e9bdb3ca1793b0bb137a8d2623aa76a47883309
yoonhyeonjeong <h_j111@naver.com>
2025-01-16 11:11:47 +0900

    fix last 수정

--- endofday/src/components/ui/DiaryCalendar.tsx
+++ endofday/src/components/ui/DiaryCalendar.tsx
@@ -73,7 +73,7 @@ const DiaryCalendar: React.FC = () => {
     return (
         <div className="flex flex-col items-center md:w-[80%] gap-10 pt-[3rem]">
             <Heading tag="h1">나의 기록</Heading>
-            <div className="overflow-hidden md:shadow-md shadow-sm rounded-[20px] ">
+            <div className="md:shadow-md shadow-sm rounded-[20px] ">
                 <Calendar
                     onChange={value => {
                         setSelectedDate(value as Date);
